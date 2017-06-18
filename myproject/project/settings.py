743f451e260e61a90f9cdf1c7732179ce6143aea
Mikael Ahonen <mikael.ahonen@hotmail.com>
2017-06-18 17:05:23 +0300

    Static file routing.

--- myproject/project/settings.py
+++ myproject/project/settings.py
@@ -145,7 +145,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
