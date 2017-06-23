2d70d663c021c72687e92852d62b5656a02ba99a
Mikael Ahonen <mikael.ahonen@hotmail.com>
2017-06-23 09:56:21 +0300

    Gitignore fix 5

--- myenv34/Lib/site-packages/whitenoise/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from .base import WhiteNoise
-
-__version__ = '2.0.6'
-
-__all__ = ['WhiteNoise']
--- myenv34/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\MIKAEL~1\WORKSP~1\myenv34\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
