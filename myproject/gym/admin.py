bfe60bc60586128fa460bcdd614c28376c3121dc
Mikael Ahonen <mikaelah@solita.fi>
2017-08-10 20:05:02 +0300

    Add workouts to admin panel.

--- myproject/gym/admin.py
+++ myproject/gym/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Excercise
-from .models import MuscleGroup
-from .models import Plan
-from .models import Routine
-from .models import RoutinePlan
-from .models import Section
+from .models import *
 
 admin.site.register(Excercise)
 admin.site.register(MuscleGroup)
@@ -14,3 +9,4 @@ admin.site.register(Plan)
 admin.site.register(Routine)
 admin.site.register(RoutinePlan)
 admin.site.register(Section)
+admin.site.register(Workout)
